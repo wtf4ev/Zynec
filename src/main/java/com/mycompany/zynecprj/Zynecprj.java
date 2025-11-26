3b1b07efbc88ef13c6afbd85e62726428bd12e4a
revaa <revspeakin@gmail.com>
2025-11-26 12:31:38 +0530

    Update Zynecprj.java

--- src/main/java/com/mycompany/zynecprj/Zynecprj.java
+++ src/main/java/com/mycompany/zynecprj/Zynecprj.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.zynecprj;
 
-/**
- *
- * @author user
- */
+
 public class Zynecprj {
 
     public static void main(String[] args) {
